08ac096b2ed99009f447efaaea0a8ec4f6aebf2b
antoinegleisberg <antoine.gleisberg@gmail.com>
2022-12-13 13:10:47 +0100

    adding videowriter to landmark detector

--- lib/landmark_detector.py
+++ lib/landmark_detector.py
@@ -8,7 +8,7 @@ mp_face_mesh = mp.solutions.face_mesh
 
 
 class LandmarkDetector:
-    def __init__(self, dynamic=False) -> None:
+    def __init__(self, dynamic=False, to_video=False) -> None:
         """Initialize the landmark detector. If dynamic is True, the detector will use the webcam"""
         self.face_mesh = mp_face_mesh.FaceMesh(
             max_num_faces=1, refine_landmarks=True, min_detection_confidence=0.5, min_tracking_confidence=0.5
@@ -16,8 +16,15 @@ class LandmarkDetector:
         self.image = None
         self.landmarks = None
         self.dynamic = dynamic
+        self.to_video = to_video
         if self.dynamic:
             self.videoCapture = cv2.VideoCapture(0)
+            self.videoWriter = cv2.VideoWriter(
+                "filename.avi",
+                cv2.VideoWriter_fourcc(*"MJPG"),
+                10,
+                (int(self.videoCapture.get(3)), int(self.videoCapture.get(4))),
+            )
 
     def set_image(self, image):
         """Set the image to be processed"""
@@ -83,12 +90,15 @@ class LandmarkDetector:
                 self.draw_landmarks()  # draw the landmarks for the first face detected
                 self.show_landmarks()
 
+            self.videoWriter.write(self.image)
+
             # Press escape to exit
             if cv2.waitKey(5) & 0xFF == 27:
                 break
         self.videoCapture.release()
+        self.videoWriter.release()
 
 
 if __name__ == "__main__":
-    landmark_detector = LandmarkDetector(dynamic=True)
+    landmark_detector = LandmarkDetector(dynamic=True, to_video=True)
     landmark_detector.run()
